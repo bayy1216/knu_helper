37b63f4c8c20cc265591c5381e3e66d3b52e15de
Hong <bayyyy@naver.com>
2023-07-20 19:29:18 +0900

    Feat: 사이트 추가

---
+++ lib/all/view/setting_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:knu_helper/common/layout/default_layout.dart';
+
+class SettingScreen extends StatelessWidget {
+  const SettingScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+      child: Column(
+        children: [],
+      ),
+    );
+  }
+}
