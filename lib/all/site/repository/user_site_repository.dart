c84af66834668143e64e380d287ab4afa04cbfc5
손홍석 <78216059+bayy1216@users.noreply.github.com>
2023-12-18 15:57:17 +0900

    [Feat]: 레거시 주석

--- lib/all/site/repository/user_site_repository.dart
+++ lib/all/site/repository/user_site_repository.dart
@@ -1,42 +1,42 @@
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import '../../../common/database/drift_database.dart';
-import '../../../notice/model/site_color.dart';
-import '../../../notice/model/site_enum.dart';
-
-
-final userSiteRepositoryProvider = Provider<UserSiteRepository>((ref) {
-  final db = ref.watch(databaseProvider);
-  return UserSiteRepository(database: db);
-});
-
-class UserSiteRepository {
-  final LocalDatabase database;
-
-  UserSiteRepository({
-    required this.database,
-  });
-
-
-  Future<List<SiteColorModel>> getSite() async {
-    final resp = await database.getSiteColors();
-    final sites = resp
-        .map((e) => SiteColorModel(
-      site: e.site,
-      hexCode: e.hexCode,
-    )).toList();
-
-    return sites;
-  }
-
-  Future<void> saveSite({required SiteColorModel model}) async {
-    await FirebaseMessaging.instance.subscribeToTopic(SiteEnum.getType[model.site]!.englishName);
-    database.createSiteColor(model.toCompanion());
-  }
-
-  Future<void> deleteSite({required SiteColorModel model}) async {
-    await FirebaseMessaging.instance.unsubscribeFromTopic(SiteEnum.getType[model.site]!.englishName);
-    database.deleteSiteColor(model.toCompanion());
-  }
-}
+// import 'package:firebase_messaging/firebase_messaging.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+//
+// import '../../../common/database/drift_database.dart';
+// import '../../../notice/model/site_color.dart';
+// import '../../../notice/model/site_enum.dart';
+//
+//
+// final userSiteRepositoryProvider = Provider<UserSiteRepository>((ref) {
+//   final db = ref.watch(databaseProvider);
+//   return UserSiteRepository(database: db);
+// });
+//
+// class UserSiteRepository {
+//   final LocalDatabase database;
+//
+//   UserSiteRepository({
+//     required this.database,
+//   });
+//
+//
+//   Future<List<SiteColorModel>> getSite() async {
+//     final resp = await database.getSiteColors();
+//     final sites = resp
+//         .map((e) => SiteColorModel(
+//       site: e.site,
+//       hexCode: e.hexCode,
+//     )).toList();
+//
+//     return sites;
+//   }
+//
+//   Future<void> saveSite({required SiteColorModel model}) async {
+//     await FirebaseMessaging.instance.subscribeToTopic(SiteEnum.getType[model.site]!.englishName);
+//     database.createSiteColor(model.toCompanion());
+//   }
+//
+//   Future<void> deleteSite({required SiteColorModel model}) async {
+//     await FirebaseMessaging.instance.unsubscribeFromTopic(SiteEnum.getType[model.site]!.englishName);
+//     database.deleteSiteColor(model.toCompanion());
+//   }
+// }
