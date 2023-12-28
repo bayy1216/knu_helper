e56a0608fb9695eae68692151ce00b41b0f7afa8
손홍석 <78216059+bayy1216@users.noreply.github.com>
2023-12-28 20:03:26 +0900

    [Feat]: 공지사이트 없을시 cowItem 추가

--- lib/notice/view/notice_screen.dart
+++ lib/notice/view/notice_screen.dart
@@ -7,6 +7,7 @@ import 'package:knu_helper/notice/components/notice_card.dart';
 import 'package:knu_helper/notice/provider/notice_provider.dart';
 import 'package:knu_helper/notice/view/search_notice_screen.dart';
 
+import '../../common/component/cow_item.dart';
 import '../../common/utils/data_utils.dart';
 import '../../favorite/provider/favorite_provider.dart';
 import '../../user/model/response/user_subscribed_site_response.dart';
@@ -41,39 +42,42 @@ class NoticeScreen extends ConsumerWidget {
           icon: const Icon(Icons.search),
         )
       ],
-      body: PaginationListView(
-        provider: noticeProvider,
-        itemBuilder: (BuildContext context, int index, model) {
-          final colorHexcode = subScribedSites
-              .firstWhere(
-                (element) => element.site == model.site,
-                orElse: () => UserSubscribedSiteModel(
-                    site: '', color: 'FFFFFF', isAlarm: false),
-              )
-              .color;
-          return Padding(
-            padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
-            child: GestureDetector(
-              onTap: () {
-                context.goNamed(
-                  NoticeWebView.routeName,
-                  pathParameters: {'url': model.url},
+      body: subScribedSites.isEmpty
+          ? const CowItem(content: '원하는 사이트를 추가해 보세요')
+          : PaginationListView(
+              provider: noticeProvider,
+              itemBuilder: (BuildContext context, int index, model) {
+                final colorHexcode = subScribedSites
+                    .firstWhere(
+                      (element) => element.site == model.site,
+                      orElse: () => UserSubscribedSiteModel(
+                          site: '', color: 'FFFFFF', isAlarm: false),
+                    )
+                    .color;
+                return Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 4.0, horizontal: 8.0),
+                  child: GestureDetector(
+                    onTap: () {
+                      context.goNamed(
+                        NoticeWebView.routeName,
+                        pathParameters: {'url': model.url},
+                      );
+                    },
+                    child: NoticeCard.fromModel(
+                      color: Color(DataUtils.stringToColorCode(colorHexcode)),
+                      model: model,
+                      isFavorite: favoriteState.contains(model.id),
+                      onStarClick: (value) {
+                        ref
+                            .read(favoriteStreamProvider.notifier)
+                            .starClick(model: model, isDelete: value);
+                      },
+                    ),
+                  ),
                 );
               },
-              child: NoticeCard.fromModel(
-                color: Color(DataUtils.stringToColorCode(colorHexcode)),
-                model: model,
-                isFavorite: favoriteState.contains(model.id),
-                onStarClick: (value) {
-                  ref
-                      .read(favoriteStreamProvider.notifier)
-                      .starClick(model: model, isDelete: value);
-                },
-              ),
             ),
-          );
-        },
-      ),
     );
   }
 }
